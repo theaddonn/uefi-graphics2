fce9901ca9599097c53e7167b1f060041beb0598
adrian <adrian.olbrich1@gmail.com>
2024-04-16 18:48:59 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
-# uefi-graphics2
-An embedded-graphics display driver for UEFI environments
+# Uefi-graphics2
+
+A blazingly fast embedded-graphics display driver for UEFI environments,
+using the [`embedded-graphics`](https://crates.io/crates/embedded-graphics) crate
+as its base.
+
+Supports:
+- [X] Double buffering
+- [X] Display resizing
+- [X] An extensive draw/render library using the
+  [`embedded-graphics`](https://crates.io/crates/embedded-graphics) crate
+
+### Why are there 2 other crates for this job?
+
+[`uefi-graphics`](https://crates.io/crates/uefi-graphics) and
+[`uefi-graphics-driver`](https://crates.io/crates/uefi-graphics-driver) 
+are 2 crates providing similar purpose,
+sadly both seem to either lack functionality or are unmaintained
+
+## Example
+
+Here is a simple example with utilising the [`uefi`](https://crates.io/crates/uefi) crate on version `0.27.0`:
+
+```rust
+#![no_main]
+#![no_std]
+
+extern crate alloc;
+
+use uefi::prelude::*;
+use uefi::proto::console::gop::GraphicsOutput;
+
+use uefi_graphics2::embedded_graphics::pixelcolor::{Rgb888, RgbColor};
+use uefi_graphics2::UefiDisplay;
+
+#[entry]
+fn main(_image_handle: Handle, mut boot_system_table: SystemTable<Boot>) -> Status {
+  uefi_services::init(&mut boot_system_table).unwrap();
+
+  // Disable the watchdog timer
+  boot_system_table
+          .boot_services()
+          .set_watchdog_timer(0, 0x10000, None)
+          .unwrap();
+
+  let boot_services = boot_system_table.boot_services();
+  
+  let gop_handle = boot_services.get_handle_for_protocol::<GraphicsOutput>().unwrap();
+  let mut gop = boot_services.open_protocol_exclusive::<GraphicsOutput>(gop_handle).unwrap();
+  
+  let mode = gop.current_mode_info();
+  let mut display = UefiDisplay::new(gop.frame_buffer(), mode);
+
+  let _ = display.fill_entire(Rgb888::CYAN);
+  display.flush();
+  
+  Status::SUCCESS
+}
+```
+
+## Contributing and Updating
+
+If any dependencies are outdated, bugs occur or features are requested,
+please notify me and create an issue.
+
+Contributing is appreciated as well, feel free to create a pull request.
---
+++ src/lib.rs
@@ -0,0 +1,120 @@
+#![no_std]
+
+extern crate alloc;
+
+use alloc::vec::Vec;
+use core::ptr::copy;
+
+// for re-export
+pub use embedded_graphics;
+use embedded_graphics::draw_target::DrawTarget;
+use embedded_graphics::geometry::{OriginDimensions, Size};
+use embedded_graphics::Pixel;
+use embedded_graphics::pixelcolor::{IntoStorage, Rgb888, RgbColor};
+use embedded_graphics::prelude::Point;
+use embedded_graphics::primitives::Rectangle;
+use uefi::proto::console::gop::{FrameBuffer, ModeInfo};
+
+pub enum UefiDisplayError {
+    UnsupportedFormat,
+}
+
+pub struct UefiDisplay {
+    frame_buffer: *mut u8,
+    double_buffer: *mut u8,
+    stride: u32,
+    size: (u32, u32),
+    // width * height * 4 (red, green, blue, reserved)
+    buffer_size: u32,
+}
+
+impl UefiDisplay {
+    pub fn new(mut frame_buffer: FrameBuffer, mode_info: ModeInfo) -> Self {
+        Self {
+            frame_buffer: frame_buffer.as_mut_ptr(),
+            double_buffer: Vec::with_capacity(mode_info.resolution().0 * mode_info.resolution().1 * 4).as_mut_ptr(),
+            stride: mode_info.stride() as u32,
+            size: (
+                mode_info.resolution().0 as u32,
+                mode_info.resolution().1 as u32,
+            ),
+            buffer_size: mode_info.resolution().0 as u32 * mode_info.resolution().1 as u32 * 4,
+        }
+    }
+
+    pub fn new_custom(frame_buffer: *mut u8, stride: u32, size: (u32, u32)) -> Self {
+        Self {
+            frame_buffer,
+            double_buffer: Vec::with_capacity((size.0 * size.1 * 4) as usize).as_mut_ptr(),
+            stride,
+            size,
+            buffer_size: size.0 * size.1 * 4,
+        }
+    }
+
+    pub fn resize(&mut self, size: (u32, u32)) -> Result<(), UefiDisplayError> {
+        self.size = (size.0, size.1);
+        self.frame_buffer = Vec::with_capacity((size.0 * size.1 * 4) as usize).as_mut_ptr();
+
+        // Reset the entire buffer because if not the existing data would be shifted around
+        self.fill_entire(Rgb888::BLACK)
+    }
+
+    pub unsafe fn resize_unsafe(&mut self, size: (u32, u32)) {
+        self.size = (size.0, size.1);
+        self.frame_buffer = Vec::with_capacity((size.0 * size.1 * 4) as usize).as_mut_ptr();
+    }
+
+    pub fn fill_entire(&mut self, color: Rgb888) -> Result<(), UefiDisplayError> {
+        self.fill_solid(
+            &Rectangle {
+                top_left: Point {
+                    x: 0,
+                    y: 0,
+                },
+                size: Size {
+                    width: self.size.0,
+                    height: self.size.1,
+                },
+            },
+            color,
+        )
+    }
+
+    pub fn flush(&mut self) {
+        unsafe { copy(self.double_buffer, self.frame_buffer, self.buffer_size as usize) }
+    }
+}
+
+impl OriginDimensions for UefiDisplay {
+    fn size(&self) -> Size {
+        Size::from(self.size)
+    }
+}
+
+impl DrawTarget for UefiDisplay {
+    type Color = Rgb888;
+    type Error = UefiDisplayError;
+
+    fn draw_iter<I>(&mut self, pixels: I) -> Result<(), Self::Error>
+        where
+            I: IntoIterator<Item=Pixel<Self::Color>>,
+    {
+        let pixels = pixels.into_iter();
+
+        for Pixel(point, color) in pixels {
+            let bytes = color.into_storage();
+            let stride = self.stride as u64;
+            let (x, y) = (point.x as u64, point.y as u64);
+
+            let index: usize = match (((y * stride) + x) * 4).try_into() {
+                Ok(index) => index,
+                Err(_) => return Err(UefiDisplayError::UnsupportedFormat),
+            };
+
+            unsafe { (self.double_buffer.add(index) as *mut u32).write_volatile(bytes) };
+        }
+
+        Ok(())
+    }
+}
